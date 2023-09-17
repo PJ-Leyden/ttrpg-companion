d82f6dd6b3ef0496158c900ed1a9a202f66c22dc
PJ Leyden <pjleyden1@gmail.com>
2023-09-16 21:36:48 -0400

    Added models file

---
+++ src/common/Table.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+export interface ITableProps {}
+
+const CharacteristicsTable: React.FC<ITableProps> = (props) => {
+
+  return (
+    
+  )
+
+
+};
+
+export default CharacteristicsTable;
---
+++ src/warhammer/models.ts
@@ -0,0 +1,16 @@
+export interface IWarhammerCharacterSheet {
+    characteristics: IWarhammerCharacteristics;
+};
+
+export interface IWarhammerCharacteristics {
+    weaponSkill: number;
+    ballisticSkill: number;
+    strength: number;
+    toughness: number;
+    initiative: number;
+    agility: number;
+    dexterity: number;
+    intelligence: number;
+    willpower: number;
+    fellowship: number;
+}
