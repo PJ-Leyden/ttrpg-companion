2ec2ccf230d727e9c1c8753de23ea03b9565e127
PJ Leyden <pjleyden1@gmail.com>
2023-09-27 00:17:28 -0400

    Added base CoreTable

---
+++ src/common/CoreTable.tsx
@@ -0,0 +1,43 @@
+import { TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+import { ReactElement } from "react";
+import { Table } from "semantic-ui-react";
+
+export interface ICoreTableProps<T> {
+    headers: ICoreTableHeader[];
+    data: T[];
+}
+
+export interface ICoreTableHeader {
+    label: string;
+    name: string;
+}
+
+const CoreTable = <T,>(props: ICoreTableProps<T>) => {
+    function expandItem<T>(item: T): ReactElement[] {
+        let cells: ReactElement[] = [];
+        let k: keyof typeof item;
+        for (k in item) {
+            cells.push(<TableCell>{k}</TableCell>);
+        }
+        return cells;
+    }
+
+    return (
+        <Table>
+            <TableHead>
+                <TableRow>
+                    {props.headers.map((header) => {
+                        return <TableCell>{header.label}</TableCell>;
+                    })}
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {props.data.map((item) => {
+                    return <TableRow>{expandItem(item)}</TableRow>;
+                })}
+            </TableBody>
+        </Table>
+    );
+};
+
+export default CoreTable;
--- src/common/NumberInputField.tsx
+++ src/common/NumberInputField.tsx
@@ -2,23 +2,27 @@ import { TextField } from "@mui/material";
 import { getIn, useFormikContext } from "formik";
 import styles from "./NumberInputField.module.css";
 export interface INumberInputFieldProps {
-  name: string;
-  className?: string;
+    name: string;
+    className?: string;
 }
 
 const NumberInputField: React.FC<INumberInputFieldProps> = (props) => {
-  const formik = useFormikContext();
-  const value = getIn(formik.values, props.name);
+    const formik = useFormikContext();
+    const value = getIn(formik.values, props.name);
 
-  const onChange:
-    | React.ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>
-    | undefined = (event) => {
-    formik.setFieldValue(props.name, event.target.value);
-  };
+    const onChange:
+        | React.ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>
+        | undefined = (event) => {
+        formik.setFieldValue(props.name, event.target.value);
+    };
 
-  return (
-    <TextField value={value} onChange={onChange} className={props.className} />
-  );
+    return (
+        <TextField
+            value={value}
+            onChange={onChange}
+            className={props.className}
+        />
+    );
 };
 
 export default NumberInputField;
