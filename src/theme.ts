dc06b29fd7299ce4ed4e398be75b86608fc83d98
PJ Leyden <pjleyden1@gmail.com>
2023-11-12 23:23:07 -0500

    Updated theme

--- src/theme.ts
+++ src/theme.ts
@@ -4,16 +4,19 @@ export const themeOptions: ThemeOptions = {
   palette: {
     mode: 'dark',
     primary: {
-      main: '#c70003',
+      main: '#0f0f0f',
     },
     secondary: {
-      main: '#9c27b0',
+      main: '#931f1d',
+    },
+    background: {
+      default: '#e3e3e3',
+      paper: '#ffffff',
     },
     text: {
-      primary: '#ffffff',
-    },
-    error: {
-      main: '#ffc107',
+      primary: '#000000',
+      secondary: '#000000',
+      disabled: '#000000',
     },
   },
 };
