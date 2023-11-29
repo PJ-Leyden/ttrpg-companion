e1af5033b8b3ce6f7187677f7111f46162b83ab2
PJ Leyden <pjleyden1@gmail.com>
2023-11-29 02:26:59 -0500

    Removed unneeded comments in index.html

--- src/common/framework/routes.tsx
+++ src/common/framework/routes.tsx
@@ -7,44 +7,44 @@ import Login from "./Login/Login";
 import { getTokenCookie } from "./Login/getTokenCookie";
 
 const initSheet: IWarhammerCharacteristics = {
-    weaponSkill: 80,
-    ballisticSkill: 75,
-    strength: 60,
-    toughness: 65,
-    agility: 45,
-    dexterity: 70,
-    initiative: 90,
-    intelligence: 40,
-    willpower: 20,
-    fellowship: 70,
-    test: "",
+	weaponSkill: 80,
+	ballisticSkill: 75,
+	strength: 60,
+	toughness: 65,
+	agility: 45,
+	dexterity: 70,
+	initiative: 90,
+	intelligence: 40,
+	willpower: 20,
+	fellowship: 70,
+	test: "",
 };
 
 export const routes: RouteObject[] = [
-    {
-        path: "/login",
-        element: <Login />,
-    },
-    {
-        path: "/signup",
-        element: <Signup />,
-    },
-    {
-        path: "/",
-        element: <MainLayout />,
-        loader: async () => {
-            const tokenCookie = getTokenCookie();
-            if (tokenCookie !== "") {
-                return redirect("/login");
-            }
+	{
+		path: "/login",
+		element: <Login />,
+	},
+	{
+		path: "/signup",
+		element: <Signup />,
+	},
+	{
+		path: "/",
+		element: <MainLayout />,
+		loader: async () => {
+			const tokenCookie = getTokenCookie();
+			if (tokenCookie === "") {
+				return redirect("/login");
+			}
 
-            return null;
-        },
-        children: [
-            {
-                path: "warhammer",
-                element: <WarhammerCharacterSheet sheet={initSheet} />,
-            },
-        ],
-    },
+			return null;
+		},
+		children: [
+			{
+				path: "warhammer",
+				element: <WarhammerCharacterSheet sheet={initSheet} />,
+			},
+		],
+	},
 ];
